8090add7b61120294ab400d155cb7b8801dfae36
Frans Andrew B. Vermeir <25-1-00442@vsu.edu.ph>
2026-03-02 09:39:01 +0800

    50A - Domino piling

---
+++ 800 elo Problems/A. Domino piling.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() { 
+    int N = 0;
+    int M = 0;
+    int area;
+    int result;
+    cin >> N >> M;
+    area = N * M;
+    result = area / 2;
+    cout << result;
+    return 0;
+}
